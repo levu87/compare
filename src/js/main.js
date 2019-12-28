7e74a1775b0b6a60211f8d7f9394d75bad047ced
Lê Vũ <vulbps06998@fpt.edu.vn>
2019-12-28 10:54:18 +0700

    html

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,40 @@ function setBackground() {
 		})
 	})
 }
+function toggleMenu() {
+	$('.mobile-toggle').on('click', function () {
+		$('.mobile-toggle').toggleClass('active')
+		$('.mobile-menu').toggleClass('active')
+		$('.back-drop').toggleClass('active')
+		$('body').toggleClass('over-hidden')
+	});
+	$('.back-drop').on('click', function () {
+		$('.mobile-toggle').removeClass('active')
+		$('.mobile-menu').removeClass('active')
+		$('.back-drop').removeClass('active')
+		$('body').removeClass('over-hidden')
+	});
+}
+function toolNavMapping() {
+	try {
+		return new MappingListener({
+			selector: ".header .main-menu",
+			mobileWrapper: ".header .mobile-menu",
+			mobileMethod: "appendTo",
+			desktopWrapper: ".header .main-wrap .right",
+			desktopMethod: "prependTo",
+			breakpoint: 1025
+		}).watch()
+	} catch (e) {}
+}
+
+function Headers(){
+	var e = 0;
+    $(window).scroll(function () {
+        var i = $(window).scrollTop();
+        $("body header").toggleClass("hide", i > e), e = $(window).scrollTop(), i > 0 ? $("header .header__top").addClass("hidden") : $("header .header__top").removeClass("hidden"),i > 0 ? $("header .header__bottom").addClass("bg") : $("header .header__bottom").removeClass("bg"), $(window).scrollTop() >= $(document).height() - $(window).height() && $(".load-more").trigger("click")
+    })
+}
 function Aos(){
 	AOS.init({
 		disable: 'phone',
@@ -29,11 +63,6 @@ function Aos(){
 		once: true, // whether animation should happen only once - while scrolling down
 		mirror: false, // whether elements should animate out while scrolling past them
 		anchorPlacement: "top-bottom" // defines which position of the element regarding to window should trigger the animation
-	});
-}
-function images(){
-	$(function(){
-		$('.panorama-view').panorama360();
 	});
 }
 function compareZoom(){
@@ -51,9 +80,273 @@ function compareZoom(){
 		}
 	  });
 }
-$(document).ready(function () {
-	setBackground()
-	Aos()
-	images()
-	compareZoom()
+function swiperInit(){
+	let HomeBanner = new Swiper('.home-banner .swiper-container', {
+        speed: 1000,
+        navigation: {
+            clickable: true,
+            nextEl: '.home-banner .swiper-next',
+            prevEl: '.home-banner .swiper-prev'
+        },
+        pagination: {
+            el: '.home-banner .swiper-pagination',
+            clickable: true
+        },
+	})
+	var galleryThumbs = new Swiper('.project-detail .gallery-thumbs', {
+		spaceBetween: 10,
+		slidesPerView: 4,
+		freeMode: true,
+		watchSlidesVisibility: true,
+		watchSlidesProgress: true,
+	  });
+	  var galleryTop = new Swiper('.project-detail .gallery-top', {
+		spaceBetween: 10,
+		navigation: {
+		  nextEl: '.gallery-top .swiper-button-next',
+		  prevEl: '.gallery-top .swiper-button-prev',
+		},
+		thumbs: {
+		  swiper: galleryThumbs
+		}
+	  });
+}
+function autorun(){
+	$('.home-utilities .cicre span').each(function () {
+	  $(this).prop('Counter',0).animate({
+		  Counter: $(this).text()
+	  }, {
+		  duration: 5000,
+		  easing: 'swing',
+		  step: function (now) {
+			  $(this).text(Math.ceil(now));
+			  $('.big').addClass('active');           
+		  }
+	  });
+	//   var something = $(".circle .item")
+	//   .css({border:  '1.6rem solid #0799d9'})
+	  
+	//   .show()        
+	//   setTimeout(function(){
+	// 	  something.css({border: '1.6rem solid #ffffff'});
+	//   },5000);
+  });
+  }
+  function showmore(){
+	$(".home-news .list-item .item").slice(0,6).show();
+	$(".view-more").click(function(e){
+	  e.preventDefault();
+	  $(".home-news .list-item .item:hidden").slice(0,3).fadeIn("slow");
+	  
+	  if($(".home-news .list-item .item:hidden").length == 0){
+		 $(".view-more").fadeOut("slow");
+		}
+	});
+  }
+  function gotop() {
+	$('#gotop').on('click', function () {
+	  $('html,body').animate({
+		scrollTop: 0
+	  }, 1000)
+	})
+  }
+  function sidebarMenu() {
+	$('.has-submenu').on('click', function () {
+		if ($(this).find('ul').is(':hidden')) {
+		$(this).find('ul').slideDown()
+		$(this).addClass('active')
+		
+		} else {
+		$(this).find('ul').slideUp()
+		$(this).removeClass('active')
+		}
+	});
+	$('.sidebar-menu .side-menu-mobile-header').on('click', function () {
+		$('.sidebar-menu .menu-list').slideToggle()
+	})
+}
+function CustomSelect() {
+	var x, i, j, selElmnt, a, b, c;
+	/*look for any elements with the class "custom-select":*/
+	x = document.getElementsByClassName("custom-select");
+	for (i = 0; i < x.length; i++) {
+	  selElmnt = x[i].getElementsByTagName("select")[0];
+	  /*for each element, create a new DIV that will act as the selected item:*/
+	  a = document.createElement("DIV");
+	  a.setAttribute("class", "select-selected");
+	  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+	  x[i].appendChild(a);
+	  /*for each element, create a new DIV that will contain the option list:*/
+	  b = document.createElement("DIV");
+	  b.setAttribute("class", "select-items select-hide");
+	  for (j = 1; j < selElmnt.length; j++) {
+		/*for each option in the original select element,
+		create a new DIV that will act as an option item:*/
+		c = document.createElement("a");
+		c.innerHTML = selElmnt.options[j].innerHTML;
+		c.setAttribute('href', selElmnt.options[j].value)
+		c.addEventListener("click", function (e) {
+		  /*when an item is clicked, update the original select box,
+		  and the selected item:*/
+		  var y, i, k, s, h;
+		  s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+		  h = this.parentNode.previousSibling;
+		  for (i = 0; i < s.length; i++) {
+			if (s.options[i].innerHTML == this.innerHTML) {
+			  s.selectedIndex = i;
+			  h.innerHTML = this.innerHTML;
+			  y = this.parentNode.getElementsByClassName("same-as-selected");
+			  for (k = 0; k < y.length; k++) {
+				y[k].removeAttribute("class");
+			  }
+			  this.setAttribute("class", "same-as-selected");
+			  break;
+			}
+		  }
+		  h.click();
+		});
+		b.appendChild(c);
+	  }
+	  x[i].appendChild(b);
+	  a.addEventListener("click", function (e) {
+		// khi hộp chọn được nhấp, đóng bất kỳ hộp chọn nào khác, và mở / đóng hộp chọn hiện tại
+		e.stopPropagation();
+		closeAllSelect(this);
+		this.nextSibling.classList.toggle("select-hide");
+		this.classList.toggle("select-arrow-active");
+	  });
+	}
+  
+	function closeAllSelect(elmnt) {
+	  /*a function that will close all select boxes in the document,
+	  except the current select box:*/
+	  var x, y, i, arrNo = [];
+	  x = document.getElementsByClassName("select-items");
+	  y = document.getElementsByClassName("select-selected");
+	  for (i = 0; i < y.length; i++) {
+		if (elmnt == y[i]) {
+		  arrNo.push(i)
+		} else {
+		  y[i].classList.remove("select-arrow-active");
+		}
+	  }
+	  for (i = 0; i < x.length; i++) {
+		if (arrNo.indexOf(i)) {
+		  x[i].classList.add("select-hide");
+		}
+	  }
+	}
+	/*if the user clicks anywhere outside the select box,
+	then close all select boxes:*/
+	document.addEventListener("click", closeAllSelect);
+  }
+function tabActive(){
+	$('.menu-list li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('.menu-list li').removeClass('active');
+		$('.tab-content').removeClass('active');
+
+		$(this).addClass('active');
+		$("#"+tab_id).addClass('active');
+	})
+}
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+            $('#imagePreview').hide();
+            $('#imagePreview').fadeIn(650);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+$("#imageUpload").change(function() {
+    readURL(this);
 });
+
+function handleFileSelect(evt) {
+	var files = evt.target.files; // FileList object
+	// Loop through the FileList and render image files as thumbnails.
+	for (var i = 0, f; f = files[i]; i++) {
+	// Only process image files.
+	if (!f.type.match('image.*')) {
+	continue;
+	}
+	var reader = new FileReader();
+	// Closure to capture the file information.
+	reader.onload = (function(theFile) {
+	return function(e) {
+	// Render thumbnail.
+	var span = document.createElement('span');
+	span.innerHTML = ['<img class="thumb" src="', e.target.result,
+	'" title="', escape(theFile.name), '"/>'].join('');
+	document.getElementById('list').insertBefore(span, null);
+	};
+	})(f);
+
+	// Read in the image file as a data URL.
+	reader.readAsDataURL(f);
+	}
+	}
+	$('#files').change(handleFileSelect);
+function textEditor(){
+	$('#summernote').summernote();
+}
+
+$(document).ready(function () {
+	swiperInit()
+	setBackground()
+	toggleMenu()
+	toolNavMapping()
+	Aos()
+	compareZoom()
+	autorun()
+	showmore()
+	Headers()
+	gotop()
+	sidebarMenu()
+	tabActive()
+	CustomSelect()
+	handleFileSelect()
+	textEditor()
+	readURL(input)
+	const listViewButton = document.querySelector('.list-view-button');
+	const gridViewButton = document.querySelector('.grid-view-button');
+	const list = document.querySelector('.list');
+
+	listViewButton.onclick = function () {
+	list.classList.remove('grid-view-filter');
+	list.classList.add('list-view-filter');
+	gridViewButton.classList.remove('active');
+	listViewButton.classList.add('active');
+	}
+
+	gridViewButton.onclick = function () {
+	list.classList.remove('list-view-filter');
+	list.classList.add('grid-view-filter');
+	gridViewButton.classList.add('active');
+	listViewButton.classList.remove('active');
+	}
+});
+
+$(document).on("click", ".browse", function() {
+  var file = $(this).parents().find(".file");
+  file.trigger("click");
+});
+$('input[type="file"]').change(function(e) {
+  var fileName = e.target.files[0].name;
+  $("#file").val(fileName);
+
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    // get loaded data and render thumbnail.
+    document.getElementById("preview").src = e.target.result;
+  };
+  // read the image file as a data URL.
+  reader.readAsDataURL(this.files[0]);
+});
+
+
